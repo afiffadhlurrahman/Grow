aed563808cb162e6bad4437ca0f8c303f04dc46a
Afif Fadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-04-24 21:16:39 +0700

    Initial Commit

---
+++ Grow Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grow Watch App
+//
+//  Created by Muhammad Afif Fadhlurrahman on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grow Watch App/GrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowApp.swift
+//  Grow Watch App
+//
+//  Created by Muhammad Afif Fadhlurrahman on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Grow_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
