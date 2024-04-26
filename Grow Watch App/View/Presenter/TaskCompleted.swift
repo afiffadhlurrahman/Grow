50c5cfb41a1715af685dfa4c66325c2d232191c0
Afif Fadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-04-26 11:28:18 +0700

    Adding folder Model and View.

---
+++ Grow Watch App/View/Presenter/TaskCompleted.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCompleted.swift
+//  Grow Watch App
+//
+//  Created by Muhammad Afif Fadhlurrahman on 26/04/24.
+//
+
+import SwiftUI
+
+struct TaskCompleted: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskCompleted()
+}
