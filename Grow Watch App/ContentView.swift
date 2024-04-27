99c6a038e136ecc8e8b59e73db2df3246c005437
rangga <rangga.yudhistira@ui.ac.id>
2024-04-27 21:45:28 +0700

    Adding Circular Progress Bar and Tree Animation

--- Grow Watch App/ContentView.swift
+++ Grow Watch App/ContentView.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel: LottieViewModel = .init()
+    @State private var progress: CGFloat = 0.0 // State untuk mengontrol progress bar
+    
+    @State private var text: String = ""
     
     var body: some View {
-        Image(uiImage: viewModel.image)
-            .resizable()
-            .scaledToFit()
-            .onAppear {
-                       self.viewModel.loadAnimation(url: URL(string: "https://assets8.lottiefiles.com/packages/lf20_Zz37yH.json")!)
-                   }
+        ZStack {
+            Color(red: 0.78, green: 0.92, blue: 0.79).ignoresSafeArea()
+            
+            // Progress Bar
+            VStack {
+                Spacer()
+                ProgressView(value: progress)
+                    .progressViewStyle(CircularProgressViewStyle(tint: Color(red: 0.44, green: 0.69, blue: 0.52))) // Mengubah warna progress bar
+                    .scaleEffect(2.5) // Memperbesar ukuran progress bar
+                    .padding()
+                    .onAppear {
+                        // Start animating the progress bar
+                        withAnimation(.linear(duration: 3)) { // Mengubah durasi menjadi 2 detik
+                            self.progress = 1.0
+                        }
+                    }
+                
+                Spacer()
+            }
+            
+            // Animasi Lottie
+            Image(uiImage: viewModel.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80)
+                .padding() // Sesuaikan dengan kebutuhan
+                .onAppear {
+                    self.viewModel.speed = 0.5
+                    self.viewModel.loadAnimation(url: URL(string: "https://lottie.host/892ef239-5e69-4385-9b46-a1d56de77971/iLbrJpNZu3.json")!)
+                    
+                }
+            
+            
+        }
     }
 }
 
